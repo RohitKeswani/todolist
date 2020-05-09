73087d3de791045afcf1caf27565ac30a4170cfb
RohitKeswani <rhareshk@gmail.com>
2020-05-09 00:25:35 -0400

    Added the logic for item addition

--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      items:[],
+      currentItem :{
+        text :'',
+        key :''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!==""){
+      const newItems =[...this.state.items, newItem];
+      this.setState({
+        item :newItems,
+        currentItem :{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+
   render(){
     return (
       <div className ="App">
+        <h3 id ="title">Your own To-Do List</h3>
         <header>
-          <form id="to-do-form">
-            <input type ="text" placeholder ="Enter your task here"/>
-            <button type ="submit" > Add Task</button>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type ="text" placeholder ="Enter your task here" value={this.state.currentItem.text} 
+            onChange={this.handleInput}/>
+            <button type ="submit">Add Task</button>
           </form>
         </header>
       </div>
